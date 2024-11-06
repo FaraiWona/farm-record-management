464bd17e3c5dc58c9376ff7c54878782584f9365
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-05 23:27:02 -0800

    creating async function for signInWithEmailAndPassword

--- farm_record_management/lib/services/auth.dart
+++ farm_record_management/lib/services/auth.dart
@@ -4,4 +4,20 @@ import 'package:logger/logger.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   var logger = Logger();
+
+  User? get currentUser => _auth.currentUser;
+
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      logger.e('Error during email sign-in: $e');
+      return null;
+    }
+  }
 }
