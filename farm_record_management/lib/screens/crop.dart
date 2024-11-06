32bd18a71da0ecb8e740050185a4aa95d46862d7
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-06 05:34:47 -0800

    importing packages in crop page

--- farm_record_management/lib/screens/crop.dart
+++ farm_record_management/lib/screens/crop.dart
@@ -1,1 +1,6 @@
-
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'update_crop_page.dart';
+import 'home_page.dart';
+import 'animal_home_page.dart';
+import 'update_animals_page.dart';
