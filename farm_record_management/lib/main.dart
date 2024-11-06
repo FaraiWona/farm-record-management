a978522ecdb5f3a0c5b76308fddb91d126ea8120
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-06 12:57:18 -0800

    importing packages and page in main dart

--- farm_record_management/lib/main.dart
+++ farm_record_management/lib/main.dart
@@ -1,1 +1,8 @@
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:farm_record_management/screens/authenticate/log_in.dart';
+import 'package:farm_record_management/screens/authenticate/sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:logger/logger.dart';
+import 'package:farm_record_management/screens/crop.dart';
+import 'package:farm_record_management/screens/home_page.dart';
