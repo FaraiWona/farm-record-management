db73afe62ed81a2269668239e30005c9a5a28533
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-05 23:40:21 -0800

    creating sign in class

--- farm_record_management/lib/screens/authenticate/sign_in.dart
+++ farm_record_management/lib/screens/authenticate/sign_in.dart
@@ -2,3 +2,10 @@ import 'package:farm_record_management/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:logger/logger.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
