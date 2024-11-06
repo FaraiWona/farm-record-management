2e8407494d31ef924d98125e238b35051b861d02
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-06 12:17:39 -0800

    declaring variables

--- farm_record_management/lib/screens/update_crop_page.dart
+++ farm_record_management/lib/screens/update_crop_page.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UpdateCropPage extends StatelessWidget {
+  final String cropId;
+  final String cropName;
+  final String plantingDate;
+  final String harvestDate;
+  final String quantity;
+  final String notes;
