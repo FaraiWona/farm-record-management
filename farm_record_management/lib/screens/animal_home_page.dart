0f9d828a81d526d4eef529658197cfe01a9d1400
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-06 10:53:38 -0800

    creating a body with animal page

--- farm_record_management/lib/screens/animal_home_page.dart
+++ farm_record_management/lib/screens/animal_home_page.dart
@@ -4,7 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class AnimalHomePage extends StatelessWidget {
   const AnimalHomePage({super.key});
 
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -15,5 +15,47 @@ class AnimalHomePage extends StatelessWidget {
             fontSize: 24,
           ),
         ),
-        backgroundColor: Colors.green, 
+        backgroundColor: Colors.green,
       ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome to the Animal Management App!',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.greenAccent,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  'Manage your animals efficiently.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+              AnimalDetailsForm(),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // will add later
+        },
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
