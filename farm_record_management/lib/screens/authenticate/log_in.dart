48c05b2369c469303d6e0aac358c0735b7fc88eb
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-06 00:06:58 -0800

    creating log in class

--- farm_record_management/lib/screens/authenticate/log_in.dart
+++ farm_record_management/lib/screens/authenticate/log_in.dart
@@ -2,3 +2,10 @@ import 'package:farm_record_management/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:logger/logger.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
