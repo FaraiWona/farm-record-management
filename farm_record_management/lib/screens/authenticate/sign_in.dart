035325686e869b0da7161286a57000e63295daa6
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-05 23:38:56 -0800

    importing necessary packages in sign in page

--- farm_record_management/lib/screens/authenticate/sign_in.dart
+++ farm_record_management/lib/screens/authenticate/sign_in.dart
@@ -1,1 +1,4 @@
-
+import 'package:farm_record_management/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:logger/logger.dart';
