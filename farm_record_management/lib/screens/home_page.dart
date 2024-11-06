ae6a1c37ff4855f2bd11fb8e5e88872c30fc7854
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-06 00:32:28 -0800

    creating a home page class

--- farm_record_management/lib/screens/home_page.dart
+++ farm_record_management/lib/screens/home_page.dart
@@ -1,3 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'crop.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Farm Management App',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const CropListPage()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(child: CropDetailsForm()),
+      ),
+    );
+  }
+}
