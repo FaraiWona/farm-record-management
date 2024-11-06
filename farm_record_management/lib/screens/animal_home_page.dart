a48924cb55dd9a402b85a16fc5b5611061efa4ae
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-06 10:37:17 -0800

    creating animal class statelesswidget

--- farm_record_management/lib/screens/animal_home_page.dart
+++ farm_record_management/lib/screens/animal_home_page.dart
@@ -1,2 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AnimalHomePage extends StatelessWidget {
+  const AnimalHomePage({super.key});
+}
