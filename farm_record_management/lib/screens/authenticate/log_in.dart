0a084f45675cf420c7db62cde2f933bfdc5af43c
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-06 00:14:19 -0800

    function for throwing errors or success text

--- farm_record_management/lib/screens/authenticate/log_in.dart
+++ farm_record_management/lib/screens/authenticate/log_in.dart
@@ -17,3 +17,43 @@ class _LoginPageState extends State<LoginPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   bool _isLoading = false;
   var logger = Logger();
+
+ Future<void> _loginWithEmailAndPassword() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      String email = _emailController.text.trim();
+      String password = _passwordController.text.trim();
+
+      setState(() {
+        _isLoading = true;
+      });
+
+      try {
+        User? user = await _auth.signInWithEmailAndPassword(email, password);
+        if (mounted) {
+          if (user != null) {
+            logger.i('Login successful');
+            Navigator.pushReplacementNamed(
+                context, '/crops'); // Navigate to CropListPage
+          } else {
+            logger.w('Error during sign-in');
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Invalid email or password')),
+            );
+          }
+        }
+      } catch (e) {
+        if (mounted) {
+          logger.e('Error during login: $e');
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Error during login: $e')),
+          );
+        }
+      } finally {
+        if (mounted) {
+          setState(() {
+            _isLoading = false;
+          });
+        }
+      }
+    }
+  }
