763696e13c462032046aea37ac30db013f3f398a
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-05 23:25:09 -0800

    inatialising auth services

--- farm_record_management/lib/services/auth.dart
+++ farm_record_management/lib/services/auth.dart
@@ -1,2 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:logger/logger.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  var logger = Logger();
+}
