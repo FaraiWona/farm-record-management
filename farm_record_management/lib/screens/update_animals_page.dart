5089a0365946e4d69713588793c0f0f070d28ced
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-06 12:29:52 -0800

    declaring variables

--- farm_record_management/lib/screens/update_animals_page.dart
+++ farm_record_management/lib/screens/update_animals_page.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class UpdateAnimalPage extends StatelessWidget {}
+class UpdateAnimalPage extends StatelessWidget {
+   final String animalId;
+  final String birthDate;
+  final String breed;
+  final String healthStatus;
+  final String species;
+  final String notes;
+
