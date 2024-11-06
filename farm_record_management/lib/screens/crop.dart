35c2652015473767eb048e2d20a9b4cfa8a3344f
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-06 13:10:00 -0800

    main

--- farm_record_management/lib/screens/crop.dart
+++ farm_record_management/lib/screens/crop.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'animal_home_page.dart';
 import 'home_page.dart';
+import 'update_crop_page.dart';
+import 'update_animals_page.dart';
 
 class CropListPage extends StatelessWidget {
   const CropListPage({super.key});
