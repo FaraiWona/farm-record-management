76b64e247a2c22fe21e9a3d37bcb4e83af21b94a
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-05 23:49:30 -0800

    creating a function for throwing errors or successful out

--- farm_record_management/lib/screens/authenticate/sign_in.dart
+++ farm_record_management/lib/screens/authenticate/sign_in.dart
@@ -9,3 +9,53 @@ class SignInPage extends StatefulWidget {
   @override
   State<SignInPage> createState() => _SignInPageState();
 }
+
+class _SignInPageState extends State<SignInPage> {
+  final AuthService _auth = AuthService();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
+  var logger = Logger();
+
+  Future<void> _signUpWithEmail() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      String email = _emailController.text.trim();
+      String password = _passwordController.text.trim();
+
+      setState(() {
+        _isLoading = true;
+      });
+
+      try {
+        User? user = await _auth.signUpWithEmailAndPassword(email, password);
+        if (user != null) {
+          if (mounted) {
+            logger.i('Sign up successful');
+            Navigator.pushReplacementNamed(
+                context, '/crops'); 
+          }
+        } else {
+          if (mounted) {
+            logger.w('Error during sign-up');
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Sign-up failed')),
+            );
+          }
+        }
+      } catch (e) {
+        if (mounted) {
+          logger.e('Error during sign-up: $e');
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Error during sign-up: $e')),
+          );
+        }
+      } finally {
+        if (mounted) {
+          setState(() {
+            _isLoading = false;
+          });
+        }
+      }
+    }
+  }
