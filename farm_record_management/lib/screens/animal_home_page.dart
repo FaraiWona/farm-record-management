2b5c7336feefe8c61967ef77e90301d132fa1167
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-06 10:41:23 -0800

    creating appbar

--- farm_record_management/lib/screens/animal_home_page.dart
+++ farm_record_management/lib/screens/animal_home_page.dart
@@ -3,4 +3,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AnimalHomePage extends StatelessWidget {
   const AnimalHomePage({super.key});
-}
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Farm Management App',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        backgroundColor: Colors.green, 
+      ),
