62bbd9c15a50530617e43216aee91f8b2dd8747b
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-06 12:59:25 -0800

    initialising the firebase and passing id values

--- farm_record_management/lib/main.dart
+++ farm_record_management/lib/main.dart
@@ -6,3 +6,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:logger/logger.dart';
 import 'package:farm_record_management/screens/crop.dart';
 import 'package:farm_record_management/screens/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var logger = Logger();
+
+  try {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyAWE9PHKzd17dCEZegBmqXIPadA2JGLExc",
+        appId: "1:575843924317:android:c116c7b470ce368b7d50b4",
+        messagingSenderId: "your-messaging-sender-id",
+        projectId: "farm-ca350",
+        authDomain: "your-auth-domain",
+        storageBucket: "your-storage-bucket",
+        measurementId: "your-measurement-id",
+      ),
+    );
