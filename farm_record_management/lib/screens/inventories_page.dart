1b67a31c6bd777f647bf8db1b4a2e9cb6020dd68
Bed-com-35-22 <Bed-com-35-22@unima.ac.mw>
2024-11-08 08:17:28 -0800

    class inventorypage

--- farm_record_management/lib/screens/inventories_page.dart
+++ farm_record_management/lib/screens/inventories_page.dart
@@ -1,3 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'crop.dart';
+
+class InventoryPage extends StatelessWidget {
+  const InventoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Inventory Management App',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        const CropListPage()), // Navigate back to CropListPage
+              );
+            },
+          ),
+        ],
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(child: InventoryDetailsForm()),
+      ),
+    );
+  }
+}
+
