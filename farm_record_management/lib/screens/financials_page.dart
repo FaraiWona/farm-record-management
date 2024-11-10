c98c34b0ca09219b03e61125e092728a2a44c222
bed-com-06-21 <bed-com-06-21@unima.ac.mw>
2024-11-10 15:20:20 +0200

    adding class financial page

--- farm_record_management/lib/screens/financials_page.dart
+++ farm_record_management/lib/screens/financials_page.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'crop.dart';
+class FinancialsPage extends StatelessWidget {
+  const FinancialsPage({super.key});
