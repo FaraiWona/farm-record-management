80e82b7b53e97b7d3d715b4609392f742d96a882
bed-com-02-21 <bed-com-02-21@unima.ac.mw>
2024-11-13 21:04:52 +0200

    created class for update inventories feature

--- farm_record_management/lib/screens/update_inventories_page.dart
+++ farm_record_management/lib/screens/update_inventories_page.dart
@@ -1,2 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UpdateInventoryPage extends StatelessWidget {
+  final String inventoryId;
+  final String item;
+  final String quantity;
+  final String purchaseDate;
+  final String notes;
+
+  const UpdateInventoryPage({
+    super.key,
+    required this.inventoryId,
+    required this.item,
+    required this.quantity,
+    required this.purchaseDate,
+    required this.notes,
+  });
+
