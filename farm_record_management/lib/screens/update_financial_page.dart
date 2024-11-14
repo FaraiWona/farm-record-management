8bbde1cd5e3589ef0fc58c8bb9f5e2596fd0122a
bed-com-02-21 <bed-com-02-21@unima.ac.mw>
2024-11-14 04:46:19 +0200

    updating financial records page

--- farm_record_management/lib/screens/update_financial_page.dart
+++ farm_record_management/lib/screens/update_financial_page.dart
@@ -18,3 +18,52 @@ class UpdateFinancialsPage extends StatefulWidget {
     required this.description,
     required this.notes,
   });
+ @override
+  _UpdateFinancialsPageState createState() => _UpdateFinancialsPageState();
+}
+
+class _UpdateFinancialsPageState extends State<UpdateFinancialsPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _transactionTypeController =
+      TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _notesController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _transactionTypeController.text = widget.transactionType;
+    _amountController.text = widget.amount;
+    _dateController.text = widget.date;
+    _descriptionController.text = widget.description;
+    _notesController.text = widget.notes;
+  }
+
+  void _updateFinancialDetails() {
+    if (_formKey.currentState!.validate()) {
+      final updatedFinancialDetails = {
+        'transactionType': _transactionTypeController.text,
+        'amount': _amountController.text,
+        'date': _dateController.text,
+        'description': _descriptionController.text,
+        'notes': _notesController.text,
+      };
+
+      FirebaseFirestore.instance
+          .collection('financials')
+          .doc(widget.financialId)
+          .update(updatedFinancialDetails)
+          .then((_) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Transaction updated successfully!')),
+        );
+        Navigator.pop(context);
+      }).catchError((error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to update transaction: $error')),
+        );
+      });
+    }
+  }
